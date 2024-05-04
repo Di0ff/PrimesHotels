69c407c234d70c5cd72d83f3269e0e501f23b04e
Dmitry Kim <119820509+Di0ff@users.noreply.github.com>
2024-05-04 18:36:01 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(Primes(11, 20))
+}
+
+func Primes(min, max int) []int {
+	var primes []int
+
+	for num := min; num <= max; num++ {
+		if isPrime(num) {
+			primes = append(primes, num)
+		}
+	}
+
+	return primes
+}
+
+func isPrime(num int) bool {
+	if num <= 1 {
+		return false
+	}
+	for i := 2; i*i <= num; i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
+}
